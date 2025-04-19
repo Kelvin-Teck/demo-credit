16440b892660ca9cf4723efaddc0d31f5cbf24c8
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2025-04-19 06:39:58 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+
+const PORT = process.env.PORT || 2000;
+
+const startApp = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
+
+startApp();
